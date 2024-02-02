6711d1e5596b6ee5953bab28150b129564ff4ab3
Jordan Johnson <jajohnson460@gmail.com>
2024-02-02 15:14:14 -0500

    Set up shot resizing for later upgrades

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,15 +5,19 @@ extends Node
 @export var shot_scene: PackedScene
 @export var ready_sprite: Texture2D
 @export var cooldown_sprite: Texture2D
+@export var initial_size: float = 0.4
 # Location node
 var location: PathFollow2D
 var sprite: Sprite2D
+var shot_size: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
     location = get_node("ShotPath/ShotLocation")
     sprite = get_node("ShotPath/ShotLocation/Sprite2D")
     sprite.texture = ready_sprite
+    sprite.scale = Vector2(initial_size, initial_size)
+    shot_size = initial_size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,6 +32,7 @@ func drop_ball():
     sprite.texture = cooldown_sprite
     var shot = shot_scene.instantiate()
     shot.position = location.global_position
+    shot.set_size(shot_size)
     add_child(shot)
 
 
--- scripts/shot.gd
+++ scripts/shot.gd
@@ -1,10 +1,18 @@
 extends RigidBody2D
 
-@export var launch_force: float = 500.0
+@export var rebound_force: float = 500.0
+const BASE_RADIUS = 32
+
+func set_size(size: float):
+    $Sprite2D.scale = Vector2(size, size)
+    var new_radius = size * BASE_RADIUS
+    $CollisionShape2D.shape.radius = new_radius
+    $VisibleOnScreenNotifier2D.rect.position = Vector2(-new_radius, -new_radius)
+    $VisibleOnScreenNotifier2D.rect.size = Vector2(new_radius * 2, new_radius * 2)
 
 func launch(centerpoint: Vector2):
     var direction = (position - centerpoint).normalized()
-    var impulse = direction * launch_force
+    var impulse = direction * rebound_force
     apply_central_impulse(impulse)
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
